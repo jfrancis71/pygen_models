9b85e2a07c34a65c9413556d96c91217c1f3fd1c
Julian Francis <julian.w.francis@gmail.com>
2024-05-10 08:47:32 +0100

    Modified to support PixelCNNLayer

--- distributions/pixelcnn.py
+++ distributions/pixelcnn.py
@@ -6,19 +6,22 @@ import pygen.layers.independent_quantized_distribution as ql
 
 
 class _PixelCNN(nn.Module):
-    def __init__(self, event_shape, layer, nr_resnet=3):
+    def __init__(self, event_shape, layer, params):
         super().__init__()
         self.event_shape = event_shape
         self.layer = layer
-        self.batch_shape = []
-        self.pixelcnn_net = pixelcnn_model.PixelCNN(nr_resnet=nr_resnet, nr_filters=160,
-            input_channels=self.event_shape[0], nr_params=layer.params_size(), nr_conditional=None)
+        self.pixelcnn_net = None
+        if params is None:
+            self.batch_shape = []
+        else:
+            self.batch_shape = params.shape[0]
+        self.params = params
 
     def log_prob(self, samples):
         if samples.size()[1:4] != torch.Size(self.event_shape):
             raise RuntimeError("sample shape {}, but event_shape has shape {}"
                 .format(samples.shape[1:4], self.event_shape))
-        logits = self.pixelcnn_net((samples*2.0)-1.0, conditional=None)
+        logits = self.pixelcnn_net((samples*2.0)-1.0, conditional=self.params)
         layer_logits = logits.permute(0, 2, 3, 1)  # B, Y, X, P where P for parameters
         permute_samples = samples.permute(0, 2, 3, 1)  # B, Y, X, C
         return self.layer(layer_logits).log_prob(permute_samples).sum(axis=[1, 2])
@@ -32,7 +35,7 @@ class _PixelCNN(nn.Module):
             sample = torch.zeros(batch_shape+self.event_shape, device=next(self.parameters()).device)
             for y in range(self.event_shape[1]):
                 for x in range(self.event_shape[2]):
-                    logits = self.pixelcnn_net((sample*2)-1, sample=True, conditional=None)[:, :, y, x]
+                    logits = self.pixelcnn_net((sample*2)-1, sample=True, conditional=self.params)[:, :, y, x]
                     pixel_sample = self.layer(logits).sample()
                     sample[:, :, y, x] = pixel_sample
         if sample_shape is None:
@@ -43,9 +46,15 @@ class _PixelCNN(nn.Module):
 
 class PixelCNNBernoulliDistribution(_PixelCNN):
     def __init__(self, event_shape, nr_resnet=3):
-        super().__init__(event_shape, bernoulli_layer.IndependentBernoulli(event_shape=event_shape[:1]), nr_resnet=nr_resnet)
+        super().__init__(event_shape,
+                         bernoulli_layer.IndependentBernoulli(event_shape=event_shape[:1]), None)
+        self.pixelcnn_net = pixelcnn_model.PixelCNN(nr_resnet=nr_resnet, nr_filters=160,
+                            input_channels=self.event_shape[0], nr_params=self.layer.params_size(), nr_conditional=None)
 
 
 class PixelCNNQuantizedDistribution(_PixelCNN):
     def __init__(self, event_shape, nr_resnet=3):
-        super().__init__(event_shape, ql.IndependentQuantizedDistribution(event_shape=event_shape[:1]), nr_resnet=nr_resnet)
+        super().__init__(event_shape,
+                         ql.IndependentQuantizedDistribution(event_shape=event_shape[:1]), None)
+        self.pixelcnn_net = pixelcnn_model.PixelCNN(nr_resnet=nr_resnet, nr_filters=160,
+                            input_channels=self.event_shape[0], nr_params=self.layer.params_size(), nr_conditional=None)

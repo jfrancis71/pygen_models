70f75ddb4400b2195e24471c97bf40ccad49aab2
Julian Francis <julian.w.francis@gmail.com>
2024-06-24 09:14:28 +0100

    Adding broadcast so you can have distribution over multiple samples with single conditional, eg, batch_shape=[] but it is conditioned, eg [16,28,28] with sample_shape=[5]

--- distributions/pixelcnn.py
+++ distributions/pixelcnn.py
@@ -11,7 +11,7 @@ class _PixelCNN(nn.Module):
         self.event_shape = event_shape
         self.layer = layer
         self.pixelcnn_net = None
-        if params is None:
+        if params is None or len(params.shape)==3:
             self.batch_shape = []
         else:
             self.batch_shape = params.shape[0]
@@ -22,7 +22,11 @@ class _PixelCNN(nn.Module):
         if samples.size()[1:4] != torch.Size(self.event_shape):
             raise RuntimeError("sample shape {}, but event_shape has shape {}"
                 .format(samples.shape[1:4], self.event_shape))
-        logits = self.pixelcnn_net((samples*2.0)-1.0, conditional=self.params)
+        params = self.params
+        if params is not None:
+            if len(params.shape) == 3:
+                params = params.unsqueeze(0).repeat(samples.shape[0], 1, 1, 1)
+        logits = self.pixelcnn_net((samples*2.0)-1.0, conditional=params)
         layer_logits = logits.permute(0, 2, 3, 1)  # B, Y, X, P where P for parameters
         permute_samples = samples.permute(0, 2, 3, 1)  # B, Y, X, C
         return self.layer(layer_logits).log_prob(permute_samples).sum(axis=[1, 2])
@@ -33,13 +37,17 @@ class _PixelCNN(nn.Module):
         else:
             batch_shape = sample_shape
         with torch.no_grad():
+            params = self.params
+            if params is not None:
+                if len(params.shape) == 3:
+                    params = params.unsqueeze(0).repeat(batch_shape[0], 1, 1, 1)
             # pylint: disable=E1101
             sample = torch.zeros(batch_shape+self.event_shape,
                 device=next(self.parameters()).device)
             for y in range(self.event_shape[1]):
                 for x in range(self.event_shape[2]):
                     logits = self.pixelcnn_net((sample*2)-1, sample=True,
-                        conditional=self.params)[:, :, y, x]
+                        conditional=params)[:, :, y, x]
                     pixel_sample = self.layer(logits).sample()
                     sample[:, :, y, x] = pixel_sample
         if sample_shape is None:

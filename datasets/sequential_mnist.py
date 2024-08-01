ce9720589c20e20320680148e66988e198987ed3
Julian Francis <julian.w.francis@gmail.com>
2024-08-01 09:57:44 +0100

    Option to vary sequence length

--- datasets/sequential_mnist.py
+++ datasets/sequential_mnist.py
@@ -6,7 +6,7 @@ class SequentialMNISTDataset(Dataset):
     """An element is a tensor of shape [4,1,28,28] representing a sequence of digit images of increasing value
     starting from a random digit and 9 wraps to 0.
     dummy_run uses small subset of MNIST, used just for checking program runs."""
-    def __init__(self, mnist_dataset, dummy_run=False):
+    def __init__(self, mnist_dataset, seq_len=3, dummy_run=False):
         if dummy_run:
             self.mnist_dataset_len = 100
         else:
@@ -14,6 +14,7 @@ class SequentialMNISTDataset(Dataset):
         mnist_digits = torch.stack([mnist_dataset[n][0] for n in range(self.mnist_dataset_len)]).float()
         mnist_labels = torch.tensor([mnist_dataset[n][1] for n in range(self.mnist_dataset_len)])
         self.digits = [mnist_digits[mnist_labels == d] for d in range(10)]
+        self.seq_len = seq_len
 
     def __len__(self):
         return self.mnist_dataset_len
@@ -22,7 +23,7 @@ class SequentialMNISTDataset(Dataset):
         d = torch.randint(low=0, high=9, size=[])
         rand_idx_sel = torch.randint(low=0, high=self.digits[d].shape[0], size=[])
         i1 = [self.digits[d][rand_idx_sel]]
-        for s in range(4):
+        for s in range(self.seq_len-1):
             d = (d + 1) % 10
             rand_idx_sel = torch.randint(low=0, high=self.digits[d].shape[0], size=[])
             i1.append(self.digits[d][rand_idx_sel])

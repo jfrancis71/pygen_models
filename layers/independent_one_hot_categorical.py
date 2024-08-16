e3226182e6597660291bc13934f4fa543fb9e052
Julian Francis <julian.w.francis@gmail.com>
2024-08-16 12:29:13 +0100

    Remove OneHotCategorical Layer. Replaced by IndependentOneHotCategorical, set event_shape=[]

--- layers/independent_one_hot_categorical.py
+++ layers/independent_one_hot_categorical.py
@@ -1,29 +1,4 @@
-"""
-Defines OneHotCategorical layer.
-"""
-
-
 import torch
-
-
-class OneHotCategorical(torch.nn.Module):
-    """Layer which accepts a tensor and returns a OneHotCategorical probability distribution.
-
-    Example::
-
-        >>> one_hot_categorical_layer = OneHotCategorical()
-        >>> distribution = one_hot_categorical_layer(torch.rand([7, 10]))
-        >>> distribution.batch_shape
-        torch.Size([7])
-        >>> distribution.sample().shape
-        torch.Size([7, 10])
-    """
-
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, logits):
-        return torch.distributions.one_hot_categorical.OneHotCategorical(logits=logits)
 
 
 class IndependentOneHotCategorical(torch.nn.Module):

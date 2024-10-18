702f12d542eaeda840e0bae51f4bd2855a9c3f81
jfrancis71 <julian.w.francis@gmail.com>
2024-10-18 10:09:04 +0100

    Removing unused code.

--- train/callbacks.py
+++ train/callbacks.py
@@ -20,30 +20,5 @@ def sample_images(distribution):
     return _fn
 
 
-class TBSequenceImageCallback:
-    # pylint: disable=R0903
-    def __init__(self, tb_writer, tb_name):
-        self.tb_writer = tb_writer
-        self.tb_name = tb_name
-
-    def __call__(self, trainer_state):
-        sample_size = 8
-        imglist = [trainer_state.trainable.sample() for _ in range(sample_size)]
-        imglist = torch.clip(torch.cat(imglist, axis=0), 0.0, 1.0)  # pylint: disable=E1101
-        grid_image = torchvision.utils.make_grid(imglist, padding=10, nrow=5)
-        self.tb_writer.add_image(self.tb_name, grid_image, trainer_state.epoch_num)
-
-
-class TBSequenceTransitionMatrixCallback:
-    # pylint: disable=R0903
-    def __init__(self, tb_writer, tb_name):
-        self.tb_writer = tb_writer
-        self.tb_name = tb_name
-
-    def __call__(self, trainer_state):
-        image = trainer_state.trainable.latent_model.markov_chain.state_transition_distribution().probs.detach().unsqueeze(0).cpu().numpy()
-        self.tb_writer.add_image(self.tb_name, image, trainer_state.epoch_num)
-
-
 import doctest
 doctest.testmod()

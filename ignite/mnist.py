a3847ce686d6f02374aa697d006b86954e9bb8a4
Julian Francis <julian.w.francis@gmail.com>
2026-05-28 08:58:16 +0000

    Refactoring and use fully connected neural network.

--- ignite/mnist.py
+++ ignite/mnist.py
@@ -1,39 +1,67 @@
-# This example is from https://github.com/pytorch/ignite/tree/master
-# See above for license.
-
 import argparse
 import torch
 from torch import nn
-from torch.optim import SGD
+import torch.nn.functional as F
+from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import random_split
 import torchvision.datasets as datasets
 from torchvision.transforms import Compose, Normalize, ToTensor
-from ignite.engine import create_supervised_evaluator, create_supervised_trainer, Events
+from ignite.engine import Events
 from ignite.metrics import Accuracy, Loss
 from ignite.utils import setup_logger
 from ignite.engine import Events, Engine
 from pygen.train import callbacks
-from pygen.neural_nets import classifier_net
-import pygen.layers.independent_categorical as layer_categorical
+from torchvision.utils import make_grid
 
+
+# https://github.com/hugobb/discreteVAE was useful for a working discrete VAE
+# using Reinforce.
 
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.vars = nn.Parameter(torch.ones([1, 28, 28], requires_grad=True))
+        self.fc1 = nn.Linear(784, 400)
+        self.fc2 = nn.Linear(400, 64)
+        self.fc3 = nn.Linear(64, 400)
+        self.fc4 = nn.Linear(400, 784)
+
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x.view(-1, 784)))
+        logits = self.fc2(h1)
+        return logits
+
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z.view(len(z), 64)))
+        return self.fc4(h3).view(-1, 1, 28, 28)
 
     def log_prob(self, x):
-        dist = torch.distributions.Bernoulli(logits=self.vars)
-        return dist.log_prob(x)
+        logits = self.encode(x)
+        z_dist = torch.distributions.bernoulli.Bernoulli(logits=logits)
+        z = z_dist.sample()
+        log_prob_z = z_dist.log_prob(z)
+        recon_logits = self.decode(z)
+        dist = torch.distributions.Bernoulli(logits=recon_logits)
+        log_prob_recon = dist.log_prob(x)
+        log_prob_z = log_prob_z.sum(-1)
+        reinforce_loss = log_prob_recon.sum(axis=[1,2,3]).detach() * log_prob_z
+        recons_log_prob = log_prob_recon.sum(axis=[1,2,3])
+        return recons_log_prob.sum(-1) + reinforce_loss.sum(-1) - reinforce_loss.detach().sum(-1)
 
-    def sample(self):
-        dist = torch.distributions.Bernoulli(logits=self.vars)
-        return dist.sample()
+    def sample(self, x):
+        latent = torch.distributions.Bernoulli(logits=self.encode(x)).sample()
+        logits = self.decode(latent)
+        dist = torch.distributions.Bernoulli(logits=logits)
+        return dist.probs
 
-mymodel = Model()
 
+def train_step(engine, batch):
+    x, y = batch
+    optimizer.zero_grad()
+    loss = -mymodel.log_prob(x)
+    loss.backward()
+    optimizer.step()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--datasets_folder", default="~/datasets")
@@ -42,51 +70,27 @@ parser.add_argument("--device", default="cpu")
 parser.add_argument("--max_epoch", type=int, default=10, help="number of epochs to train (default: 10)")
 args = parser.parse_args()
 
+mymodel = Model()
 transform = Compose([ToTensor(), lambda x: (x > 0.5).float()])
 dataset = datasets.MNIST(args.datasets_folder, train=True, download=True, transform=transform)
 data_split = [55000, 5000]
 train_dataset, validation_dataset = random_split(dataset, data_split)
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
 val_loader = DataLoader(validation_dataset, batch_size=32, shuffle=False)
-#model = classifier_net.ClassifierNet(mnist=True)
-mymodel.to(args.device)  # Move model before creating optimizer
-optimizer = SGD(mymodel.parameters(), lr=.001)
+optimizer = Adam(mymodel.parameters(), lr=.001)
 criterion = nn.CrossEntropyLoss()
 
-
-# Training step function
-def train_step(engine, batch):
-    x, y = batch
-    optimizer.zero_grad()
-    #y_hat = mymodel.log_prob(torch.tensor(x, dtype=torch.int))
-    y_hat = mymodel.log_prob(x)
-    loss = -y_hat.sum()
-    loss.backward()
-    optimizer.step()
-    return loss.item()
-
-# Ignite trainer
 trainer = Engine(train_step)
-
-#trainer = create_supervised_trainer(model, optimizer, criterion, device=args.device)
 trainer.logger = setup_logger("trainer")
-
-val_metrics = {"nll": Loss(criterion)}
-#evaluator = create_supervised_evaluator(model, metrics=val_metrics, device=args.device)
-#evaluator.logger = setup_logger("evaluator")
 example_valid_images = next(iter(torch.utils.data.DataLoader(validation_dataset, batch_size=25)))[0].to(args.device)
 tb_writer = SummaryWriter(args.tb_folder)
 
 @trainer.on(Events.EPOCH_COMPLETED)
 def log_results(engine):
-#    evaluator.run(train_loader)
-#    metrics = evaluator.state.metrics
-#    tb_writer.add_scalar("training/avg_loss", metrics["nll"], engine.state.epoch)
-#    evaluator.run(val_loader)
-#    metrics = evaluator.state.metrics
-#    tb_writer.add_scalar("validation/avg_loss", metrics["nll"], engine.state.epoch)
     if tb_writer is not None:
-        image = mymodel.sample()
+        images = mymodel.sample(example_valid_images)
+        images = torch.cat([example_valid_images, images], dim=0)
+        image = make_grid(images, nrow=25, padding=10)
         tb_writer.add_image("valid_images", image, engine.state.epoch)
 
 trainer.run(train_loader, max_epochs=args.max_epoch)
